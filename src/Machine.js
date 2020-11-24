b6dc37812e6270e0f8a34102413e219d62ca3490
REMA <princeoffori@icloud.com>
2020-11-23 23:07:54 -0500

    Test-Driven Developing practise

--- src/Machine.js
+++ src/Machine.js
@@ -1,17 +1,74 @@
 class Machine {
   constructor() {
-    this.snacks = []
+    this.snacks = [];
+    this.balance = 0;
+    this.acceptedBills = [500,100,50,20,10];
+    //this.change = {change: 0}
   }
 
   seeSelections() {
-    return this.snacks
+    let availableOptions = [];
+
+    Object.keys(this.snacks).forEach(i => {
+      if (this.snacks[i] != null) {
+        availableOptions.push(this.snacks[i]);
+      }
+    })
+
+    return availableOptions;
   }
 
   stock(inventory) {
     if(inventory == undefined) {
       throw Error("please do not troll. you cannot stock nothing.")
+    }else
+    {
+      inventory.forEach(i => {
+        this.snacks.push(i);
+      })
     }
-    this.snacks = inventory
+  }
+
+  insert(money){
+    if(money == null || money == 0){
+      return `You cannot deposit that amount. Your current balance is ${this.balance}$.`;
+    }else
+    {
+      this.balance += money;
+      return `You have deposited ${money}$. Your current balance is ${this.balance}$.`
+    }
+  }
+
+  selectItem(code) {
+    let cSnack = this.snacks.filter(i => i.name == code );
+
+    if (cSnack.length < 1) {
+      return `Item ${code} is not available. Your current balance is ${this.balance}$.`
+    } else if (this.balance <= cSnack[0].price) {
+      let sDiff = cSnack[0].price - this.balance;
+      return `Your deposit is insufficient.  Please add $ ${sDiff} for this item`
+    }else {
+      //implement a feature to delete the item once bought
+      let cObject = {};
+      this.balance -= cSnack[0].price;
+
+      cObject[`item`] = cSnack[0].name;
+
+      cObject['change'] = [];
+      this.acceptedBills.forEach(i => {
+        if (this.balance >= i) {
+          this.balance -= i;
+          cObject['change'].push(i);
+        }
+      })
+
+      return cObject;
+    }
+  }
+
+  cancel(){
+    let cBal = {change:100};
+    return cBal;
   }
 }
 
--- test/Machine.test.js
+++ test/Machine.test.js
@@ -2,14 +2,54 @@ const Machine = require('../src/Machine')
 
 describe('The vending machine', () => {
   it('is initialized with no items', () => {
-    // SEAT
     // setup
     const vendingMachine = new Machine();
 
-    // exercise & assert
-    expect(vendingMachine.seeSelections()).toEqual([])
+    // exercise
+    //none
+    
+    //assert
+    expect(vendingMachine.seeSelections()).toEqual([]);
 
-    // teardown, not needed
+    //teardown
+  })
+
+  it('return how much money the customer has when inserting money', () => {
+    //setup
+    const vendingMachine = new Machine();
+
+    //exercise
+    let cMoney0 = vendingMachine.insert(null);
+    let cMoney1 = vendingMachine.insert(0);
+    let cMoney2 = vendingMachine.insert(10);
+    let cMoney3 = vendingMachine.insert(20);
+    let cMoney4 = vendingMachine.insert(50);
+    let cMoney5 = vendingMachine.insert(100);
+    let cMoney6 = vendingMachine.insert(500);
+
+    //assert
+    expect(cMoney0).toEqual('You cannot deposit that amount. Your current balance is 0$.');
+    expect(cMoney1).toEqual('You cannot deposit that amount. Your current balance is 0$.');
+    expect(cMoney2).toEqual('You have deposited 10$. Your current balance is 10$.');
+    expect(cMoney3).toEqual('You have deposited 20$. Your current balance is 30$.');
+    expect(cMoney4).toEqual('You have deposited 50$. Your current balance is 80$.');
+    expect(cMoney5).toEqual('You have deposited 100$. Your current balance is 180$.');
+    expect(cMoney6).toEqual('You have deposited 500$. Your current balance is 680$.');
+
+    //teardown
+  })
+
+  it('unavailable items will return that the item is unavailable', () => {
+    //setup
+    const vendingMachine = new Machine();
+
+    //exercise
+    let choice = vendingMachine.selectItem(400);
+
+    //assert
+    expect(choice).toEqual('Item 400 is not available. Your current balance is 0$.');
+  
+    //teardown
   })
 
   it('can stock one snack', () => {
@@ -21,30 +61,65 @@ describe('The vending machine', () => {
     }
 
     // exercise
-    vendingMachine.stock([snack])
+    vendingMachine.stock([snack]);
 
     // assert
-    expect(vendingMachine.seeSelections()).toEqual([snack])
+    expect(vendingMachine.seeSelections()).toEqual([snack]);
+
+    //teardown
   })
-
-  // someone attempts to stock without inventory
-  // it('stocks nothing if there is no inventory passed', () => {
-  //   // setup
-  //   const vendingMachine = new Machine()
-
-  //   // exercise
-  //   vendingMachine.stock()
-
-  //   // assertion
-  //   expect(vendingMachine.seeSelections()).toEqual([])
-  // })
 
   it('displays an error if no inventory comes with stocking', () => {
     // setup
-    const vendingMachine = new Machine()
-    const displayMessage = "please do not troll. you cannot stock nothing."
+    const vendingMachine = new Machine();
 
     // exercise & assert
-    expect(() => vendingMachine.stock()).toThrow(displayMessage)
+    expect(() => vendingMachine.stock()).toThrow("please do not troll. you cannot stock nothing.");
+
+    //teardown
+  })
+
+  it('display a error when there is an insufficient balace', () =>{
+    //setup
+    const vendingMachine = new Machine();
+    const Message = 'Your deposit is insufficient.  Please add $ 20 for this item'
+
+    //exercise
+    vendingMachine.stock([{name:'Cheedos', price: 20}])
+
+    //assert
+    expect(vendingMachine.selectItem('Cheedos')).toEqual(Message)
+    
+    //teardown
+  })
+
+  it('return change and the item bought when selecting a valid item', () => {
+    //setup
+    const vendingMachine = new Machine();
+
+    //exercise
+    vendingMachine.stock([{name:'Doritos', price: 500}]);
+    vendingMachine.insert(550);
+    var expectation = { item: 'Doritos', change: [50]};
+
+    //assert
+    expect(vendingMachine.selectItem('Doritos')).toEqual(expectation);
+    
+
+    //teardown
+  })
+
+  it('return cash back when customer cancel trasaction', () => {
+    //setup
+    const vendingMachine = new Machine;
+    var C_expect = {change: 100}
+
+    //exercise
+    vendingMachine.cancel();
+
+    //assert
+    expect(vendingMachine.cancel())
+
+    //teardown
   })
 })
